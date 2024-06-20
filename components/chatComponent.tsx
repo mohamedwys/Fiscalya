c1436e24d6e19b65683a24a23b99e822451f548b
Mehdi Faid <mehdi.faid@accenture.com>
2024-06-21 01:16:21 +0400

    ajout integration API pour chat, correction affichage chat

--- components/chatComponent.tsx
+++ components/chatComponent.tsx
@@ -2,20 +2,53 @@
 
 import { Input } from "@/components/ui/input"
 import { useChat } from "ai/react"
-import { useRef, useEffect } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import Balancer from "react-wrap-balancer"
+import axios from 'axios'
 
 export function Chat() {
 
-    const { messages, input, handleInputChange, handleSubmit } = useChat({
-        api: 'api/ex3',
-        onError: (e) => {
-            console.log(e)
-        }
-    })
+    // const { messages, handleInputChange, handleSubmit } = useChat({
+    //     api: 'api/ex3',
+    //     onError: (e) => {
+    //         console.log(e)
+    //     }
+    // })
+
     const chatParent = useRef<HTMLUListElement>(null)
+    const [messageHistory, setMessageHistory] = useState<string[]>([]);
+    const [input, setInput] = useState<string>('');
+
+    const askGpt = (event: React.FormEvent) => {
+        event.preventDefault();
+    
+        // Ajoutez le message de l'utilisateur à l'historique des messages
+        setMessageHistory([...messageHistory, `User: ${input}`]);
+    
+        // Envoyez une requête à l'API GPT-3
+        axios.post('http://localhost:8090/ai/message?sessionId=665cc64bcf8921d4a4044645'
+            + '&message=' + input, {})
+        .then((response) => {
+            // Ajoutez la réponse de l'API à l'historique des messages
+            setMessageHistory([...messageHistory, `AI: ${response.data.message}`]);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+        // Réinitialisez l'input
+        setInput('');
+    };
 
     useEffect(() => {
+        axios.get("http://localhost:8090/ai/user-active-chat-session?userId=665cc64bcf8921d4a4044645")
+        .then((response) => {
+            // console.log(response.data.messages)
+            setMessageHistory(response.data.messages); // Mettez à jour l'état de messageHistory avec les données reçues
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+
         const domNode = chatParent.current
         if (domNode) {
             domNode.scrollTop = domNode.scrollHeight
@@ -41,10 +74,45 @@ export function Chat() {
                     </h3>
                 </div>
 
-            <section className="p-4">
-                <form onSubmit={handleSubmit} className="flex w-full max-w-3xl mx-auto items-center">
-                    <Input className="flex-1 min-h-[40px]" placeholder="Type your question here..." type="text" value={input} onChange={handleInputChange} />
-                    <button className="ml-2 relative inline-flex h-12 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 " type="submit">
+
+            <section className="container px-0 pb-10 pt-5 flex flex-col flex-grow gap-4 mx-auto max-w-3xl">
+            <ul>
+                {messageHistory.map((m, index) => {
+                console.log('Test role avant'); // Ajoutez cette ligne pour vérifier les valeurs de role et content
+                    const [role, content] = m.split(': ');
+                    console.log('Test role : ' + role + " ; " + content); // Ajoutez cette ligne pour vérifier les valeurs de role et content
+                    return (
+                        <div key={index}>
+                            {role === 'User' ? (
+                                <li className="flex flex-row">
+                                    <div className="rounded-xl p-4 bg-background shadow-md flex">
+                                        <p className="text-primary">{content}</p>
+                                    </div>
+                                </li>
+                            ) : (
+                                <li className="flex flex-row-reverse">
+                                    <div className="rounded-xl p-4 bg-background shadow-md flex w-3/4">
+                                        <p className="text-primary">{content}</p>
+                                    </div>
+                                </li>
+                            )}
+                        </div>
+                    );
+                })}
+            </ul>
+            </section>
+
+            <section className="p-4 mb-10">
+                <form onSubmit={askGpt} className="flex w-full max-w-3xl mx-auto items-center">
+                    <input className="flex-1 min-h-[40px] bg-neutral-200 rounded-xl p-3 border" 
+                    placeholder="Type your question here..." 
+                    type="text" 
+                    value={input} 
+                    onChange={event => setInput(event.target.value)} // Ajout du gestionnaire d'événements onChange
+                    />
+                    <button className="ml-2 relative inline-flex h-12 overflow-hidden rounded-full p-[2px] 
+                    focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 " 
+                    type="submit">
                         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#2C3E50_0%,#004AAD_50%,#fd746c_100%)]" />
                         <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-neutral-200 px-6 py-1 text-sm font-bold text-zinc-500 backdrop-blur-3xl">
                         Submit
@@ -53,7 +121,7 @@ export function Chat() {
                 </form>
             </section>
 
-            <section className="container px-0 pb-10 flex flex-col flex-grow gap-4 mx-auto max-w-3xl">
+            {/* <section className="container px-0 pb-10 flex flex-col flex-grow gap-4 mx-auto max-w-3xl">
                 <ul ref={chatParent} className="h-1 p-4 flex-grow bg-muted/50 rounded-lg overflow-y-auto flex flex-col gap-4">
                     {messages.map((m, index) => (
                         <div key={index}>
@@ -73,7 +141,7 @@ export function Chat() {
                         </div>
                     ))}
                 </ul >
-            </section>
+            </section> */}
         </main>
     )
 }
