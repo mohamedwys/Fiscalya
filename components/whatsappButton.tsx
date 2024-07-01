bc45572a2a3b5b617f764fc8e14a690aa1794c2a
mohamed lamane <m.w.lamane@gmail.com>
2024-07-01 22:46:49 +0400

    ok

--- components/whatsappButton.tsx
+++ components/whatsappButton.tsx
@@ -4,12 +4,12 @@ const WhatsappButton: React.FC = () => {
   const handleWhatsappClick = () => {
     if (typeof navigator !== 'undefined' && navigator.userAgent.includes('WhatsApp')) {
       // WhatsApp is installed, open the app with a pre-filled message
-      window.open('https://wa.me/971525866003', '_blank');
-    } //else {
-      // WhatsApp is not installed, open the web version in a new tab
-      //window.open('https://wa.me/971525866003&text=Hello, I need assistance.', '_blank');
-      //'https://web.whatsapp.com/send?phone=+971525866003&text=Hello, I need assistance.'
-      //}
+      window.open('whatsapp://send?phone=+971525866003&text=Hello, I need assistance.', '_blank');
+     } //else {
+    //   // WhatsApp is not installed, open the web version in a new tab
+    //   window.open('https://web.whatsapp.com/send?phone=+971525866003&text=Hello, I need assistance.', '_blank');
+      
+    // }
   };
   return (
     <div className="fixed bottom-4 right-4 bg-green-500 text-white p-3 rounded-full cursor-pointer" onClick={handleWhatsappClick}>
