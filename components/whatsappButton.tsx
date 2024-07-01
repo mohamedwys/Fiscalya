e4dc0440a58e6fa26ce6ecb2c3b07709b6b84ce5
mohamed lamane <m.w.lamane@gmail.com>
2024-07-01 22:54:33 +0400

    ok

--- components/whatsappButton.tsx
+++ components/whatsappButton.tsx
@@ -2,17 +2,21 @@ import { FaWhatsapp } from 'react-icons/fa';
 
 const WhatsappButton: React.FC = () => {
   const handleWhatsappClick = () => {
-    if (typeof navigator !== 'undefined' && navigator.userAgent.includes('WhatsApp')) {
-      // WhatsApp is installed, open the app with a pre-filled message
-      window.open('whatsapp://send?phone=+971525866003&text=Hello, I need assistance.', '_blank');
-     } //else {
+    window.open('whatsapp://send?phone=+971525866003&text=Hello, I need assistance.', '_blank');
+
+    // if (typeof navigator !== 'undefined' && navigator.userAgent.includes('WhatsApp')) {
+    //   // WhatsApp is installed, open the app with a pre-filled message
+    //   window.open('whatsapp://send?phone=+971525866003&text=Hello, I need assistance.', '_blank');
+    //  } 
+     //else {
     //   // WhatsApp is not installed, open the web version in a new tab
     //   window.open('https://web.whatsapp.com/send?phone=+971525866003&text=Hello, I need assistance.', '_blank');
       
     // }
   };
   return (
-    <div className="fixed bottom-4 right-4 bg-green-500 text-white p-3 rounded-full cursor-pointer" onClick={handleWhatsappClick}>
+    <div className="fixed bottom-4 right-4 bg-green-500 text-white p-3 rounded-full cursor-pointer" 
+    onClick={handleWhatsappClick}>
       <FaWhatsapp size={24} />
     </div>
   );
